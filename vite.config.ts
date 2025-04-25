73fcc7e5c526112790322172d2e778416b2ffdab
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2025-04-24 22:14:48 -0400

    Added cargo and tauri cli, allowing desktop app development

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 5173,
+    strictPort: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
